cd3bc86e482b7ac0142bbcbc93bd68054f4c1654
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-02-22 16:44:40 +0900

    cloned LOGIN code

--- LOGIN/app.js
+++ LOGIN/app.js
@@ -1,10 +1,30 @@
 const loginForm = document.getElementById("login-form");
-
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function handleLoginBtnClick() {
-  console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+  paintGreetings(savedUsername);
+}
