7070f2f64bea5f4830226a65544a6f2209d3e9fb
whereisrmsqhs <whereisrmsqhs@gmail.com>
2024-02-24 23:04:42 +0900

    structure clock and login of MAIN

---
+++ MAIN/js/login.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector("#login-form");
+const input = loginForm.querySelector(".typeName");
+
+function paintWelcome(username) {
+  const hiElement = document.querySelector(".hi");
+  console.log(hiElement);
+  hiElement.innerText = `Hi! ${username}`;
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+  const username = input.value;
+  localStorage.setItem("username", username);
+  loginForm.classList.add("hidden");
+  paintWelcome(username);
+}
+
+loginForm.addEventListener("submit", handleLogin);
+
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  loginForm.classList.remove("hidden");
+} else {
+  paintWelcome(savedUsername);
+}
